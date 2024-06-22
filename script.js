7f1cdf83d8b8e77bdc47227f6c3315c7401e62fd
AbhinavMannuSingh <abhinavmannu1234@gmail.com>
2024-06-22 12:42:52 +0530

    deploy

---
+++ script.js
@@ -0,0 +1,19 @@
+const bar = document.getElementById('bar');
+const close = document.getElementById('close');
+const nav = document.getElementById('navbar');
+
+
+
+if (bar && nav) {
+
+    bar.addEventListener('click', () => {
+        
+        nav.classList.toggle('active');
+    });
+} 
+
+if(close){
+    close.addEventListener('click',()=>{
+        nav.classList.remove('active');
+    })
+}
